0d1c08efec2fc655d17b5cd4441080d87799d6a7
tempname1533 <pedro_de_sousa@outlook.com>
2020-09-09 16:15:05 +0200

    Errors Corrigired

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,12 @@
-import React from "react"
-import { Foot, Span } from "./styles"
+import React, { useState } from "react"
+import { Foot, Span, SocialContainer } from "./styles"
 import { graphql, useStaticQuery } from "gatsby"
 import { AiFillGithub, AiOutlineMail } from "react-icons/ai"
 
 const Footer = () => {
+  const [color, setColor] = useState("white")
+  const [color2, setColor2] = useState("white")
+
   const data = useStaticQuery(graphql`
     query {
       logo: file(relativePath: { eq: "logoWhite.png" }) {
@@ -20,14 +23,24 @@ const Footer = () => {
         up one day at a time.
       </Span>
 
-      <div>
-        <a href="https://github.com/P3drodeSousa" target="blank">
-          <AiFillGithub size={30} color="white" />
-        </a>
-        <a href="https://github.com/P3drodeSousa" target="blank">
-          <AiOutlineMail size={30} color="white" />
-        </a>
-      </div>
+      <SocialContainer>
+        <div
+          onMouseEnter={() => setColor("#7510F7")}
+          onMouseLeave={() => setColor("white")}
+        >
+          <a href="https://github.com/P3drodeSousa" target="blank">
+            <AiFillGithub size={30} color={color} />
+          </a>
+        </div>
+        <div
+          onMouseEnter={() => setColor2("#7510F7")}
+          onMouseLeave={() => setColor2("white")}
+        >
+          <a href="mailto:contact@pesousa.ch">
+            <AiOutlineMail size={30} color={color2} />
+          </a>
+        </div>
+      </SocialContainer>
 
       <Span copy>© {new Date().getFullYear()} Pedro de Sousa</Span>
     </Foot>
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -7,12 +7,33 @@ export const Foot = styled.footer`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 25rem;
+  padding: 15rem 15rem 7rem 15rem;
   text-align: center;
 `
 
 export const Span = styled.span`
-  margin-top: 2rem;
+  margin: 3rem;
   font-size: ${props => (props.copy ? "1.6rem" : "2.5rem")};
   opacity: 0.7;
+`
+export const SocialContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > div {
+    padding: 1rem;
+    border-radius: 50%;
+    background-color: transparent;
+    color: white;
+    margin: 1.5rem 1rem;
+    border: 2px solid rgba(255, 255, 255, 0.3);
+    transition: all 0.3s ease;
+
+    &:hover {
+      background: white;
+      color: #7510f7;
+      border: 2px solid white;
+    }
+  }
 `
